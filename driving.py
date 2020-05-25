1fb0ecd3813bc001ec1e52996a3fd2a2e8142f17
Jerry Hsu <cba5530@gmail.com>
2020-05-25 23:13:19 +0800

    ass US part

--- driving.py
+++ driving.py
@@ -2,7 +2,13 @@ country = input('Where r u from:')
 age = input('Please input your age:')
 age = int(age)
 if country == 'Taiwan':
-	if age >=18:
+	if age >= 18:
 		print('You can have license')
 	else:
 		print('You cannot have license')
+elif country == 'USA':
+	if  age >= 16:
+		print('You can have license')
+	else:
+		print('You cannot have license')
+		pass
