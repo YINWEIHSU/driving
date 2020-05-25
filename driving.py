0cf42d53fba74be5603d2977532f9b3138966679
Jerry Hsu <cba5530@gmail.com>
2020-05-25 23:10:21 +0800

    add Taiwan

---
+++ driving.py
@@ -0,0 +1,8 @@
+country = input('Where r u from:')
+age = input('Please input your age:')
+age = int(age)
+if country == 'Taiwan':
+	if age >=18:
+		print('You can have license')
+	else:
+		print('You cannot have license')
